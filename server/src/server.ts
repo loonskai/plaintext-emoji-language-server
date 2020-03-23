f2fb517346dd95ca3ef79436218820d13a084e3e
Siarhei Lunski <loonskai@gmail.com>
2020-03-23 17:51:10 +0300

    Create EmojiLanguageServer class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,57 +1,113 @@
 import {
-	createConnection,
-	TextDocuments,
-	ProposedFeatures,
-	InitializeParams,
 	CompletionItem,
-	CompletionItemKind,
+	CompletionList,
+	CompletionParams,
+	createConnection,
+	Diagnostic,
+	DidChangeTextDocumentParams,
+	Hover,
+	IConnection,
+	InitializeParams,
+	InitializeResult,
+	IPCMessageReader,
+	IPCMessageWriter,
+	TextDocumentContentChangeEvent,
 	TextDocumentPositionParams,
+	CompletionItemKind
 } from 'vscode-languageserver';
+import { TextDocumentSyncKind } from 'vscode-languageserver-protocol';
 import axios from 'axios';
 
-let connection = createConnection(ProposedFeatures.all);
-let documents: TextDocuments = new TextDocuments();
-
-connection.onInitialize((params: InitializeParams) => {
-	return {
-		capabilities: {
-			textDocumentSync: documents.syncKind,
-			completionProvider: {
-				// triggerCharacters: [':'],
-				resolveProvider: true
-			}
-		}
-	};
-});
-
-connection.onCompletion(
-	async (_textDocumentPosition: TextDocumentPositionParams): Promise<CompletionItem[]> => {
-		const { textDocument } = _textDocumentPosition;
-		const doc = documents.get(textDocument.uri);
-		if (!doc) return [];
-		
-		const text = doc.getText();
-		const words = text.match(/\b[a-zA-Z]{2,}/g) || [];
-		const lastWord = words[words.length - 1];
-
-		const res = await axios.get('https://www.emojidex.com/api/v1/search/emoji', {
-			params: { code_cont: lastWord.trim() }
-		});
-		const { emoji } = res.data;
-		if (!emoji) return [];
-
-		return emoji.filter((item: any) => item.unicode && item.unicode.length === 5).map((item: any) => ({
-			label: `${item.moji} ${item.code}`,
-			kind: CompletionItemKind.Text,
-			detail: item.code,
-			insertText: item.moji
-		}));
+class EmojiLanguageServer {
+	private readonly connection: IConnection;
+	private openDocuments: { [documentUri: string]: TextDocumentContentChangeEvent };
+	
+	constructor() {
+		this.connection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+		this.openDocuments = {};
 	}
-);
+	
+	public start(): void {
+		this.initializeConnection();
+		this.connection.listen();
+	}
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
-documents.listen(connection);
+	private initializeConnection(): void {
+		const { connection } = this;
+		connection.onInitialize(this.onInitialize);
 
-// Listen on the connection
-connection.listen();
+		connection.onDidChangeTextDocument(this.onDidChangeTextDocument);
+
+		connection.onCompletion(this.onCompletion);
+		connection.onCompletionResolve(this.onCompletionResolve);
+		// connection.onHover(this.onHover);
+	}
+	
+	private onInitialize = (params: InitializeParams): InitializeResult => {
+		return {
+			capabilities: {
+				textDocumentSync: TextDocumentSyncKind.Full,
+				hoverProvider: true,
+				completionProvider: {
+				resolveProvider: true,
+				// triggerCharacters: [':']
+				}
+			}
+		};
+	}
+
+	private onDidChangeTextDocument = async (event: DidChangeTextDocumentParams): Promise<void> => {
+		const {	contentChanges, textDocument	} = event;
+		const {	uri	} = textDocument;
+		const diagnostics: Diagnostic[] = [];
+		this.openDocuments[uri] = contentChanges[0];
+		this.connection.sendDiagnostics({uri, diagnostics});
+	}
+
+	private onHover = async (event: TextDocumentPositionParams): Promise<Hover | void> => {
+		const {	position, textDocument	} = event;
+		const {	uri	} = textDocument;
+		const document = this.openDocuments[uri];
+		let hoverInfo;
+		try {
+			// hoverInfo = await getHoverInfo(position, document);
+		} catch (e) {
+
+		}
+		return hoverInfo;
+	}
+
+	private onCompletion = async (completionParams: CompletionParams): Promise<CompletionItem[] | CompletionList | null> => {
+		const { textDocument } = completionParams;
+		const { uri } = textDocument;
+		const document = this.openDocuments[uri];
+		let items = null;
+		try {
+			const { text } = document;
+			const words = text.match(/\b[a-zA-Z]{2,}/g) || [];
+			const lastWord = words[words.length - 1];
+
+			const res = await axios.get('https://www.emojidex.com/api/v1/search/emoji', {
+				params: { code_cont: lastWord.trim() }
+			});
+
+			const { emoji } = res.data;
+			return emoji.filter((item: any) => item.unicode && item.unicode.length === 5).map((item: any) => ({
+				label: `${item.moji} ${item.code}`,
+				kind: CompletionItemKind.Text,
+				detail: item.code,
+				insertText: item.moji
+			}));
+		} catch (e) {
+
+		}
+		return items;
+	}
+
+	private onCompletionResolve = (completionItem: CompletionItem): CompletionItem => {
+		return completionItem;
+	}
+}
+
+const server = new EmojiLanguageServer();
+server.start();
